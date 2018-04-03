bae447137b9c12ffb4f595e88e8b18ac533f3c0e
Pierre plagnes <pierre.plagnes@gmail.com>
2018-04-03 17:09:31 +0200

    readme

---
+++ CDBReaderTests/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("CDBReaderTests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("CDBReaderTests")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("102bd851-9872-4da6-bea5-7ca048986aeb")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CDBReader
+https://ci.appveyor.com/api/projects/status/y5f1rde486tsjocl?svg=true
 CDBReader is a small .net helper that allows to read CDB files.
